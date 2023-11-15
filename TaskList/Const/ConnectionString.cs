55f5ae1e393a5d97a45792450eee6b4023046849
OniDem1 <spaindecraft17@gmail.com>
2023-11-15 13:04:23 +0700

    *

---
+++ TaskList/Const/ConnectionString.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskList.Const
+{
+    static class ConnectionString
+    {
+        static public string connectionString = "Host=45.8.96.144;Username=gen_user;Password=h@;35;/n;$8~\\z;Database=taxi_db";
+    }
+}
--- TaskList/MainPage.xaml.cs
+++ TaskList/MainPage.xaml.cs
@@ -1,203 +1,50 @@
-﻿using TaskList.Entity;
+﻿using Npgsql;
+using TaskList.Const;
+using TaskList.Entity;
 
 namespace TaskList
 {
     public partial class MainPage : ContentPage
     {
-        List<TaskEntity> TaskList = new()
-        {
-            new TaskEntity
-            {
-                Name = "Помыть посуду",
-                Description = "Посуду помыть",
-            },
-            new TaskEntity
-            {
-                Name = "Почистить картошку",
-                Description = "Картошку почистить",
-            },
-            new TaskEntity
-            {
-                Name = "Подмести пол",
-                Description = "Пол подмести",
-            },
-            new TaskEntity
-            {
-                Name = "Помыть посуду",
-                Description = "Посуду помыть",
-            },
-            new TaskEntity
-            {
-                Name = "Почистить картошку",
-                Description = "Картошку почистить",
-            },
-            new TaskEntity
-            {
-                Name = "Подмести пол",
-                Description = "Пол подмести",
-            },
-            new TaskEntity
-            {
-                Name = "Помыть посуду",
-                Description = "Посуду помыть",
-            },
-            new TaskEntity
-            {
-                Name = "Почистить картошку",
-                Description = "Картошку почистить",
-            },
-            new TaskEntity
-            {
-                Name = "Подмести пол",
-                Description = "Пол подмести",
-            },
-            new TaskEntity
-            {
-                Name = "Помыть посуду",
-                Description = "Посуду помыть",
-            },
-            new TaskEntity
-            {
-                Name = "Почистить картошку",
-                Description = "Картошку почистить",
-            },
-            new TaskEntity
-            {
-                Name = "Подмести пол",
-                Description = "Пол подмести",
-            },
-            new TaskEntity
-            {
-                Name = "Помыть посуду",
-                Description = "Посуду помыть",
-            },
-            new TaskEntity
-            {
-                Name = "Почистить картошку",
-                Description = "Картошку почистить",
-            },
-            new TaskEntity
-            {
-                Name = "Подмести пол",
-                Description = "Пол подмести",
-            },
-            new TaskEntity
-            {
-                Name = "Помыть посуду",
-                Description = "Посуду помыть",
-            },
-            new TaskEntity
-            {
-                Name = "Почистить картошку",
-                Description = "Картошку почистить",
-            },
-            new TaskEntity
-            {
-                Name = "Подмести пол",
-                Description = "Пол подмести",
-            },
-            new TaskEntity
-            {
-                Name = "Помыть посуду",
-                Description = "Посуду помыть",
-            },
-            new TaskEntity
-            {
-                Name = "Почистить картошку",
-                Description = "Картошку почистить",
-            },
-            new TaskEntity
-            {
-                Name = "Подмести пол",
-                Description = "Пол подмести",
-            },
-            new TaskEntity
-            {
-                Name = "Помыть посуду",
-                Description = "Посуду помыть",
-            },
-            new TaskEntity
-            {
-                Name = "Почистить картошку",
-                Description = "Картошку почистить",
-            },
-            new TaskEntity
-            {
-                Name = "Подмести пол",
-                Description = "Пол подмести",
-            },
-            new TaskEntity
-            {
-                Name = "Помыть посуду",
-                Description = "Посуду помыть",
-            },
-            new TaskEntity
-            {
-                Name = "Почистить картошку",
-                Description = "Картошку почистить",
-            },
-            new TaskEntity
-            {
-                Name = "Подмести пол",
-                Description = "Пол подмести",
-            },
-            new TaskEntity
-            {
-                Name = "Помыть посуду",
-                Description = "Посуду помыть",
-            },
-            new TaskEntity
-            {
-                Name = "Почистить картошку",
-                Description = "Картошку почистить",
-            },
-            new TaskEntity
-            {
-                Name = "Подмести пол",
-                Description = "Пол подмести",
-            },
-            new TaskEntity
-            {
-                Name = "Помыть посуду",
-                Description = "Посуду помыть",
-            },
-            new TaskEntity
-            {
-                Name = "Почистить картошку",
-                Description = "Картошку почистить",
-            },
-            new TaskEntity
-            {
-                Name = "Подмести пол",
-                Description = "Пол подмести",
-            },
-            new TaskEntity
-            {
-                Name = "Помыть посуду",
-                Description = "Посуду помыть",
-            },
-            new TaskEntity
-            {
-                Name = "Почистить картошку",
-                Description = "Картошку почистить",
-            },
-            new TaskEntity
-            {
-                Name = "Подмести пол",
-                Description = "Пол подмести",
-            }
-    };
+        List<TaskEntity> TaskList = new();
+
+        NpgsqlCommand command = new();
+        NpgsqlConnection con = new();
 
         public MainPage()
         {
             InitializeComponent();
-            TaskListView.ItemsSource = TaskList;
+            LoadData();
         }
 
 
         private void CreateTask_Clicked(object sender, EventArgs e)
         {
             Navigation.PushModalAsync(new CreateTaskPage());
+        }
+
+        private async void LoadData()
+        {
+            try
+            {
+                con = new(ConnectionString.connectionString);
+                await con.OpenAsync();
+                command = new($"SELECT * FROM task", con);
+                var reader = await command.ExecuteReaderAsync();
+                while (reader.Read())
+                {
+                    TaskEntity Task = new();
+                    Task.Name = (string)reader["taskname"];
+                    Task.Description = (string)reader["taskdescription"];
+                    TaskList.Add(Task);
+                }
+                await con.CloseAsync();
+                //TaskListView.ItemsSource = TaskList;
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Error", ex.Message, "OK");
+            }
         }
     }
 }
